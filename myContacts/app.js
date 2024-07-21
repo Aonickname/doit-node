d1b0e7897b0d0ec9a8cc07421f5c86bd0d4cea41
sebins <150359972+Aonickname@users.noreply.github.com>
2024-07-22 05:32:28 +0900

    익스프레스 라우팅

--- myContacts/app.js
+++ myContacts/app.js
@@ -4,11 +4,33 @@ const app = express();
 app.get("/", (req, res) => {
     res.send(`Hello, Node!`);
 });
+
+
+// 연락처 가져오기
 app.get("/contacts", (req, res) => {
     res.send("Contatcts Page");
 });
 
-app.listen(3000, () =>{
-    console.log("���� ���� ��");
-})
+//새 연락처 추가하기
+app.post("/contacts", (req, res) => {
+    res.send("Create Contacts");
+});
 
+// 연락처 1개 가져오기
+app.get("/contacts/:id", (req, res) =>{
+    res.send(`View Contacts for ID: ${req.params.id}`);
+});
+
+// 연락처 수정하기
+app.put("/contacts/:id", (req, res) =>{
+    res.send(`Update Contact for ID : ${req.params.id}`);
+});
+
+// 연락처 삭제하기
+app.delete("/contacts/:id", (req, res) => {
+    res.send(`Delete Contatct for ID : ${req.params.id}`);
+});
+
+app.listen(3000, () =>{
+    console.log("서버 실행 중");
+})
